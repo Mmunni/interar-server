2a8a6aad96d8e2352b2d4e51feeb6c88daf0dcac
Mmunni <masumamunni20@gmail.com>
2022-11-10 11:03:53 +0600

    env file added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 // const jwt = require('jsonwebtoken');
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,10 +12,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ufiao9v.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try{
+        const serviceCollection = client.db('interar').collection('services');
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err));
+
 app.get('/', (req, res) => {
-    res.send('itrain server is running')
+    res.send('interar server is running')
 })
 
 app.listen(port, () => {
-    console.log(`itrain server running on ${port}`);
+    console.log(`interar server running on ${port}`);
 })
