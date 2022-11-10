1af06097e67118f4fa8c5e5a70f507fdb78fc5e6
Mmunni <masumamunni20@gmail.com>
2022-11-10 17:12:23 +0600

    home page services added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 // const jwt = require('jsonwebtoken');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -23,11 +23,23 @@ async function run() {
     try{
         const serviceCollection = client.db('interar').collection('services');
 
-        app.get('/services', async (req, res) => {
+        app.get('/servicesAll', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        });
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
         });
     }
     finally{
